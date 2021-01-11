d04bfe4c93525913367cde70566e23b7573e0e92
SkyStef <sky.stef@yahoo.com>
2021-01-11 20:15:54 +0200

    Add files via upload
    Enjoy ! :)

---
+++ FreeRTOS_Rev_Counter.ino
@@ -0,0 +1,87 @@
+#include <Arduino_FreeRTOS.h>
+#include <semphr.h>
+#include<SoftwareSerial.h>
+                
+SoftwareSerial mySerial(11, 10); // RX | TX
+
+void Serial_Print_Task(void* pvParameters);
+void Bluetooth_Task(void* pvParameters);
+void Count_RPM_Task(void* pvParameters);
+
+SemaphoreHandle_t xSerialSemaphore=NULL;
+
+int RPM = 0;  
+int LOW_toggle_count = 0;
+char RPM_[10];
+
+void setup()
+{  
+    
+  Serial.begin(9600);
+  mySerial.begin(9600);
+
+  if (xSerialSemaphore == NULL)
+    xSerialSemaphore = xSemaphoreCreateMutex();
+  else if ((xSerialSemaphore) != NULL)
+    xSemaphoreGive(xSerialSemaphore);
+  
+  attachInterrupt(1, Sensor_ISR, CHANGE); 
+  
+  xTaskCreate(Serial_Print_Task, "Serial_Print_Task", 100, NULL, 1, NULL);  
+  xTaskCreate(Bluetooth_Task, "Bluetooth_Task", 100, NULL, 1, NULL);  
+  xTaskCreate(Count_RPM_Task, "Count_RPM_Task", 100, NULL, 1, NULL);
+   
+  vTaskStartScheduler();
+}
+
+void loop()
+{
+}
+
+void Serial_Print_Task(void* pvParameters)
+{        
+  while(1)
+  {
+    xSemaphoreTake(xSerialSemaphore, portMAX_DELAY);
+    Serial.println(RPM);
+
+    xSemaphoreGive(xSerialSemaphore);
+    vTaskDelay(1);
+  }
+}
+
+void Bluetooth_Task(void* pvParameters)
+{        
+ const TickType_t Delay = 200 / portTICK_PERIOD_MS;
+
+  while(1)
+   {
+     xSemaphoreTake(xSerialSemaphore, portMAX_DELAY);
+     
+     mySerial.write(RPM_);
+     mySerial.write("\n");
+     xSemaphoreGive(xSerialSemaphore);
+     vTaskDelay(Delay);
+   }
+}
+
+void Count_RPM_Task(void* pvParameters)
+{        
+  const TickType_t Delay = 200 / portTICK_PERIOD_MS;
+  
+  while(1)
+   {
+     xSemaphoreTake(xSerialSemaphore, portMAX_DELAY);
+     RPM = LOW_toggle_count * 300;
+     itoa(RPM, RPM_, 10);
+     LOW_toggle_count = 0;
+     
+     xSemaphoreGive(xSerialSemaphore);
+     vTaskDelay(Delay);
+   }
+}
+
+void Sensor_ISR()
+{
+  LOW_toggle_count++;
+}
